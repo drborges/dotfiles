e496fbec4bf4738b66bff6bfc4bc6ae0f383a22a
Diego Borges <diegoborges@Diegos-MBP.home>
2015-01-24 14:52:56 -0200

    Removes source on nonexistent file

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -2,6 +2,5 @@
 if [ -d $HOME/.zsh.after/ ]; then
   if [ "$(ls -A $HOME/.zsh.after/)" ]; then
     for config_file ($HOME/.zsh.after/*.zsh) source $config_file
-    for config_file (./zsh.after/*.zsh) source $config_file
   fi
 fi
